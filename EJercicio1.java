a7cc18ed214fec5429ea4990ec7a3c0631b78788
Raknar9 <102548553+Raknar9@users.noreply.github.com>
2022-09-16 17:04:40 +0200

    Add files via upload

--- EJercicio1.java
+++ EJercicio1.java
@@ -1,17 +1,29 @@
 public class EJercicio1 {
     public static void main(String[] args) {
         System.out.println("¡Hola, MIGUEL!");
-        ejercicio2();
+        System.out.printf("\n \n \n" );
         ejercicio3();
+        System.out.printf("\n \n \n" );
         ejercicio4();
+        System.out.printf("\n \n \n" );
+        ejercicio5();
+        System.out.printf("\n \n \n" );
+        ejercicio6();
+        System.out.printf("\n \n \n" );
+        ejercicio7();
+        System.out.printf("\n \n \n" );
+        ejercicio10();
+        System.out.printf("\n \n \n" );
+
+
     }
 
-    public static void ejercicio2  ( ){
+    public static void ejercicio3  ( ){
         System.out.printf("Primera \n");
         System.out.printf("Segunda \n");
         System.out.printf("Tercera \n");
     }
-    public static void ejercicio3  ( ){
+    public static void ejercicio4  ( ){
 
         System.out.printf("********* \n");
         System.out.printf("      *** \n");
@@ -23,7 +35,7 @@ public class EJercicio1 {
 
 
     }
-    public static void ejercicio4  ( ){
+    public static void ejercicio5  ( ){
         int x=0;
         while(x<=4)
         {
@@ -31,9 +43,32 @@ public class EJercicio1 {
             x++;
         }
     }
-    public static void ejercicio5  ( ) {
-        
+    public static void ejercicio6  ( ) {
+        int x=9;
+        while((x<10)&&(x>=0))
+        {
+            System.out.printf("" + x +"\n");
+            x--;
+        }
     }
+    public static void ejercicio7  ( ){
+        System.out.printf("    *     \n");
+        System.out.printf("   ***    \n");
+        System.out.printf("  *****   \n");
+        System.out.printf("   ***    \n");
+        System.out.printf("    *     \n");
+    }
+    public static void ejercicio10  ( ){
+
+
+        System.out.print("¡Tres! ");
+        System.out.print("¡Dos! ");
+        System.out.println("¡Uno! \n");
+        // System.out.println("¡Adelante!");
+
+        System.out.println("¡Adelante!");
+    }
+
 
 
 }
