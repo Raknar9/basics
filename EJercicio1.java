01d26114ea04cf3c2db0f63df26dbe324f98c612
Raknar9 <102548553+Raknar9@users.noreply.github.com>
2022-09-16 16:27:11 +0200

    Add files via upload

---
+++ EJercicio1.java
@@ -0,0 +1,39 @@
+public class EJercicio1 {
+    public static void main(String[] args) {
+        System.out.println("¡Hola, MIGUEL!");
+        ejercicio2();
+        ejercicio3();
+        ejercicio4();
+    }
+
+    public static void ejercicio2  ( ){
+        System.out.printf("Primera \n");
+        System.out.printf("Segunda \n");
+        System.out.printf("Tercera \n");
+    }
+    public static void ejercicio3  ( ){
+
+        System.out.printf("********* \n");
+        System.out.printf("      *** \n");
+        System.out.printf("      *** \n");
+        System.out.printf("      *** \n");
+        System.out.printf("***   *** \n");
+        System.out.printf("***   *** \n");
+        System.out.printf("********* \n");
+
+
+    }
+    public static void ejercicio4  ( ){
+        int x=0;
+        while(x<=4)
+        {
+            System.out.printf("" + x +"\n");
+            x++;
+        }
+    }
+    public static void ejercicio5  ( ) {
+        
+    }
+
+
+}
